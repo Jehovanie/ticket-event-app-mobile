7dcce69bd1ebafafac71d47f34c7c6188274c6ac
Jehovanie <jehovanieram@gmail.com>
2025-04-30 16:47:26 +0200

    Create ui Home Screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,110 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import NoResults from "@/components/NoResult";
+import { Card, FeaturedCard } from "@/components/Cards";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import Filters from "@/components/Filters";
+import { useState } from "react";
 
 const Index = () => {
+	const properties: any = [1, 2, 3, 4];
+	const loading = false;
+	const lastestPropertiesLoading = false;
+	const latestProperties: any = [1, 2, 3, 4, 5, 6];
+
+	const handleCardPress = () => {};
+
+	const [search, setSearch] = useState("");
+	const [isSearchMode, setIsSearchMode] = useState(false);
+
+	const handleSearch = () => {};
+	const toggleSearchMode = () => {
+		setIsSearchMode((state) => !state);
+	};
+
 	return (
-		<View>
-			<Text className="text-3xl mt-2 font-poppins"> Home...</Text>
-		</View>
+		<SafeAreaView className="bg-white flex-1">
+			<FlatList
+				data={properties}
+				renderItem={({ item }) => <Card />}
+				numColumns={2}
+				contentContainerClassName="pb-32"
+				columnWrapperClassName="flex gap-5 px-5"
+				showsVerticalScrollIndicator={false}
+				ListEmptyComponent={
+					loading ? <ActivityIndicator size="large" className="text-primary-300 mt-5" /> : <NoResults />
+				}
+				ListHeaderComponent={
+					<View>
+						<View className="px-5 bg-primary pb-10 rounded-bl-3xl rounded-br-3xl mb-5">
+							<View className="flex flex-row items-center justify-between my-5">
+								<View className="flex flex-row items-center">
+									<Text className="text-3xl font-poppins-extrabold text-white">Home</Text>
+								</View>
+								<Image source={icons.search} tintColor={"#FFFFFF"} className="size-6" />
+							</View>
+
+							<View className="flex flex-row items-center justify-between w-full mb-5">
+								<View className="relative h-[450px] w-full mx-auto bg-primary rounded-3xl">
+									<Image source={images.imageiDragons} className="h-full w-full rounded-2xl" />
+
+									<View className="absolute left-0 right-0 bottom-0 h-1/5 px-4 rounded-3xl z-10">
+										<Text className="font-poppins-extrabold md:font-poppins-bold sm:font-poppins-semibold text-white text-4xl md:text-2xl sm:text-xl">
+											Imagine Dragons
+										</Text>
+										<View className="flex flex-row sm:flex-col justify-between items-center mt-2">
+											<Text className="font-poppins-medium text-white text-xl">
+												Los Angeles, CA 90001, USA
+											</Text>
+											<Text className=" font-poppins-medium text-white text-xl">
+												From: $145.0
+											</Text>
+										</View>
+									</View>
+								</View>
+							</View>
+						</View>
+
+						<View className="px-5 mb-5">
+							<View className="flex flex-row items-center justify-between">
+								<Text className="text-xl font-poppins-semibold text-primary">This Week</Text>
+								<TouchableOpacity>
+									<Text className="text-base font-poppins-medium text-primary">See all</Text>
+								</TouchableOpacity>
+							</View>
+
+							{lastestPropertiesLoading ? (
+								<ActivityIndicator size="large" className="text-primary-300 mt-5" />
+							) : !latestProperties || latestProperties.length === 0 ? (
+								<NoResults />
+							) : (
+								<FlatList
+									data={latestProperties}
+									renderItem={({ item }) => <FeaturedCard />}
+									keyExtractor={(item) => item.toString()}
+									horizontal
+									bounces={false}
+									showsHorizontalScrollIndicator={false}
+									contentContainerClassName="gap-5 mt-5"
+								/>
+							)}
+						</View>
+						<View className="px-5">
+							<View className="flex flex-row items-center justify-between mb-4">
+								<Text className="text-xl font-poppins-semibold text-primary">Our Recomendation</Text>
+								<TouchableOpacity>
+									<Text className="text-base font-poppins-medium text-primary">See all</Text>
+								</TouchableOpacity>
+							</View>
+							<Filters />
+						</View>
+					</View>
+				}
+			/>
+		</SafeAreaView>
 	);
 };
 
---
+++ components/Cards.tsx
@@ -0,0 +1,71 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+interface Props {
+	onPress?: () => void;
+}
+
+export const FeaturedCard = ({ onPress }: Props) => {
+	const image = "";
+
+	const rating = 1.1;
+	const name = "Maitre Gims";
+	const address = "Paris France";
+	const price = 150;
+
+	return (
+		<TouchableOpacity onPress={onPress} className="flex flex-col items-start w-60 h-52 relative">
+			<Image source={images.eventLadies} className="size-full rounded-2xl" />
+			<Image source={images.cardGradient} className="size-full rounded-2xl absolute bottom-0" />
+
+			<View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
+				<Image source={icons.star} className="size-3.5" />
+				<Text className="text-xs font-rubik-bold text-primary-300 ml-1">{rating}</Text>
+			</View>
+
+			<View className="flex flex-col items-start absolute bottom-5 inset-x-5">
+				<Text className="text-xl font-poppins-semibold text-white" numberOfLines={1}>
+					{name}
+				</Text>
+				<Text className="text-base font-rubik text-white" numberOfLines={1}>
+					{address}
+				</Text>
+
+				<View className="flex flex-row items-center justify-between w-full">
+					<Text className="text-xl font-rubik-extrabold text-white">${price}</Text>
+					<Image source={icons.heart} className="size-5" />
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
+};
+
+export const Card = () => {
+	const image = "";
+	const rating = 1.1;
+	const name = "La Fouinne";
+	const address = "Paris France";
+	const price = 280;
+	const date = "2023-10-10";
+	const time = "10:00 AM";
+
+	return (
+		<TouchableOpacity className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative">
+			<View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+				<Image source={icons.star} className="size-2.5" />
+				<Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">1.5</Text>
+			</View>
+
+			<Image source={images.eventLadies} className="w-full h-60 rounded-lg" resizeMode="cover" />
+
+			<View className="flex flex-col mt-2">
+				<Text className="text-base font-poppins-bold text-black-300">{name}</Text>
+				<Text className="text-xs font-poppins text-black-100">{address}</Text>
+				<Text className="text-base font-rubik-bold">
+					{date} - {time}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
+};
